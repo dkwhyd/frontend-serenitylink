c2107bdd559f63e0481014144a6dc921a0e1c59c
dkwhyd <dikiwhyd99@gmail.com>
2023-12-01 21:04:58 +0700

    edit dashboard for user

--- src/pages/Dashboard/index.jsx
+++ src/pages/Dashboard/index.jsx
@@ -1,21 +1,25 @@
-import { Routes, Route } from 'react-router-dom';
-import ListReport from '../../components/ListReport';
+import { useSelector } from 'react-redux';
+import { Routes, Route, Navigate } from 'react-router-dom';
+import DashboardUser from '../../components/dashboardUser';
+import Login from '../../components/loginForm';
+
 import Sidebar from '../../components/sideBar';
 import TopBar from  '../../components/topBar';
 import menus from '../menus';
-import Report from '../Report';
+import Report from '../../components/newReport';
 export default function Dashboard() {
+  const auth = useSelector(state=>state.auth)
   return (
     <div className="flex flex-col h-screen bg-gray-200">
       <TopBar />
 
-      <div className="flex flex-row">
+      <div className="flex flex-row h-100">
         <Sidebar menus={menus} />
 
         <Routes>
-        <Route path="*" element={<ListReport />} />
+        <Route path="/*" element={auth.user?<DashboardUser/> : <Login/>  } />
 
-          <Route path="report/*" element={<Report />} />
+          <Route path="report/new" element={<Report />} />
         </Routes>
       </div>
     </div>
