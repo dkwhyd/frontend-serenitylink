65860244df8d1cf201e17a015efa87cf71d49672
dkwhyd <dikiwhyd99@gmail.com>
2023-12-01 21:03:23 +0700

    add form for new report

--- src/pages/menus.js
+++ src/pages/menus.js
@@ -1,16 +1,22 @@
 const menus = [
-  { icon: '../home.png', label: 'home', id: '', route: '/' },
+  { icon: '../../home.png', label: 'home', id: '', route: '/' },
   {
-    icon: '../dashboard.png',
+    icon: '../../dashboard.png',
     label: 'dashboard',
     id: 'dashboard',
     route: '/dashboard',
   },
   {
-    icon: '../report.png',
-    label: 'report',
+    icon: '../../report.png',
+    label: 'My Report',
     id: 'report',
     route: '/dashboard/report',
+  },
+  {
+    icon: '../../report.png',
+    label: 'New Report',
+    id: 'newreport',
+    route: '/dashboard/report/new',
   },
 ];
 export default menus;
