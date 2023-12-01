8cdbce95503339a85ab68c4d03caf1fe20e8cbac
dkwhyd <dikiwhyd99@gmail.com>
2023-12-01 20:56:00 +0700

    add Topbar

--- src/components/topBar.jsx
+++ src/components/topBar.jsx
@@ -5,7 +5,7 @@ import { FaUser } from 'react-icons/fa';
 export default function TopBar() {
   const auth = useSelector((state) => state.auth);
   return (
-    <div className="h-10 flex justify-between flex-cols px-10 bg-gray-100">
+    <div className="h-10 flex justify-between flex-cols px-10 bg-gray-100 top-0 sticky z-40">
       <div>
         <h3>SerenityLink</h3>
       </div>
