974858c2a80a2a324e27cdcf3b8737a68bb2bdbe
D-oxys <sanuzumaki12@gmail.com>
2023-12-02 10:09:29 +0700

    mengahapus dashboard user dan mengganti dengan kode sebelumnya, menggunakan dashboard layout, dashboard layout bisa di gunakan dengan mengimport component di folder dashboard dan gunakan <dashboardLayout>component</dashboardlayout>

--- src/pages/Dashboard/index.jsx
+++ src/pages/Dashboard/index.jsx
@@ -1,31 +1,19 @@
-
 import { useSelector } from 'react-redux';
-import { Routes, Route, Navigate } from 'react-router-dom';
-import DashboardUser from '../../components/dashboardUser';
+import { Routes, Route } from 'react-router-dom';
 import Login from '../../components/loginForm';
-
-import Sidebar from '../../components/sideBar';
-import TopBar from  '../../components/topBar';
-import menus from '../menus';
+import DashboardLayout from '../../components/dashboard/dashboardLayout';
 import Report from '../../components/newReport';
 import DetailReport from '../../components/detailReport';
+import SearchAndListReport from '../../components/user/searchAndListReport';
 export default function Dashboard() {
-  const auth = useSelector(state=>state.auth)
+  const auth = useSelector((state) => state.auth);
   return (
-    <div className="flex flex-col h-screen bg-gray-200">
-      <TopBar />
-
-      <div className="flex flex-row h-100">
-        <Sidebar menus={menus} />
-
-        <Routes>
-        <Route path="/*" element={auth.user?<DashboardUser/> : <Login/>  } />
-
-          <Route path="report/new" element={<Report />} />
-          <Route path="report/detail/:id" element={<DetailReport />} />
-
-        </Routes>
-      </div>
-    </div>
+    <DashboardLayout>
+      <Routes>
+        <Route path='/*' element={auth.user ? <SearchAndListReport /> : <Login />} />
+        <Route path='report/new' element={<Report />} />
+        <Route path='report/detail/:id' element={<DetailReport />} />
+      </Routes>
+    </DashboardLayout>
   );
 }
