815b780a16347c39350169c326b19a8b58509c7a
dkwhyd <dikiwhyd99@gmail.com>
2023-12-03 00:13:33 +0700

    fix login condition

--- src/components/loginForm.jsx
+++ src/components/loginForm.jsx
@@ -19,63 +19,85 @@ const LoginForm = () => {
 
     try {
       const { data } = await axios.post('http://localhost:5500/login', form);
-      console.log(data);
-      const { user, token } = data;
-      navigate('/dashboard');
-      dispatch(userLogin(user, token));
+      if (data.status === 'ok') {
+        const { user, token } = data;
+        navigate('/dashboard');
+        dispatch(userLogin(user, token));
+      }
     } catch (error) {
-      window.alert('Terjadi kesalahan saat mencoba untuk login. Silakan coba lagi.', error);
+      window.alert(
+        'Terjadi kesalahan saat mencoba untuk login. Silakan coba lagi.',
+        error,
+      );
     }
   };
 
   return (
     <div>
       {/* sans style */}
-      <section className='bg-gradient-calm-elegant h-screen'>
+      <section className="bg-gradient-calm-elegant h-screen">
         <FloatingButton />
-        <div className='mx-auto flex h-screen flex-col items-center justify-center px-6 py-8 lg:py-0'>
-          <div className='w-full rounded-lg border bg-white shadow sm:max-w-md md:mt-0 xl:p-0'>
-            <div className='space-y-4 p-6 sm:p-8 md:space-y-6'>
-              <h1 className='text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl'>Sign in to your account</h1>
-              <form className='space-y-4 md:space-y-6' onSubmit={handleLogin}>
+        <div className="mx-auto flex h-screen flex-col items-center justify-center px-6 py-8 lg:py-0">
+          <div className="w-full rounded-lg border bg-white shadow sm:max-w-md md:mt-0 xl:p-0">
+            <div className="space-y-4 p-6 sm:p-8 md:space-y-6">
+              <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl">
+                Sign in to your account
+              </h1>
+              <form className="space-y-4 md:space-y-6" onSubmit={handleLogin}>
                 <div>
-                  <label htmlFor='email' className='mb-2 block text-sm font-medium text-gray-900'>
+                  <label
+                    htmlFor="email"
+                    className="mb-2 block text-sm font-medium text-gray-900"
+                  >
                     Email
                   </label>
                   <input
-                    type='email'
-                    id='email'
-                    name='email'
-                    className='focus:outline-none focus:ring-1 focus:ring-primary-600 focus:border-primary-600 block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-gray-900 placeholder-gray-400 sm:text-sm'
-                    placeholder='email'
+                    type="email"
+                    id="email"
+                    name="email"
+                    className="focus:outline-none focus:ring-1 focus:ring-primary-600 focus:border-primary-600 block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-gray-900 placeholder-gray-400 sm:text-sm"
+                    placeholder="email"
                     required={true}
                     value={form.email}
-                    onChange={(e) => setForm({ ...form, email: e.target.value })}
+                    onChange={(e) =>
+                      setForm({ ...form, email: e.target.value })
+                    }
                   />
                 </div>
                 <div>
-                  <label htmlFor='password' className='mb-2 block text-sm font-medium text-gray-900'>
+                  <label
+                    htmlFor="password"
+                    className="mb-2 block text-sm font-medium text-gray-900"
+                  >
                     Password
                   </label>
                   <input
-                    type='password'
-                    id='password'
-                    name='password'
-                    placeholder='••••••••'
-                    className='focus:outline-none focus:ring-1 focus:ring-primary-600 focus:border-primary-600 block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-gray-900 placeholder-gray-400 sm:text-sm'
+                    type="password"
+                    id="password"
+                    name="password"
+                    placeholder="••••••••"
+                    className="focus:outline-none focus:ring-1 focus:ring-primary-600 focus:border-primary-600 block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-gray-900 placeholder-gray-400 sm:text-sm"
                     required={true}
                     value={form.password}
-                    onChange={(e) => setForm({ ...form, password: e.target.value })}
-                    autoComplete='on'
+                    onChange={(e) =>
+                      setForm({ ...form, password: e.target.value })
+                    }
+                    autoComplete="on"
                   />
                 </div>
 
-                <button type='submit' className='bg-primary-600  hover:bg-primary-700 focus:ring-primary-800 w-full rounded-lg px-5 py-2.5 text-center text-sm font-medium text-white focus:outline-none focus:ring-4'>
+                <button
+                  type="submit"
+                  className="bg-primary-600  hover:bg-primary-700 focus:ring-primary-800 w-full rounded-lg px-5 py-2.5 text-center text-sm font-medium text-white focus:outline-none focus:ring-4"
+                >
                   Sign in
                 </button>
-                <p className='text-sm font-light text-gray-500'>
+                <p className="text-sm font-light text-gray-500">
                   Don’t have an account yet?{' '}
-                  <Link to='/register' className='text-primary-600 font-medium hover:underline'>
+                  <Link
+                    to="/register"
+                    className="text-primary-600 font-medium hover:underline"
+                  >
                     Sign up
                   </Link>
                 </p>
