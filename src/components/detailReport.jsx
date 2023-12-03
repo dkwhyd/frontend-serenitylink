bf48b626726c2c28936e347f60148a2968642f21
dkwhyd <dikiwhyd99@gmail.com>
2023-12-03 23:47:53 +0700

    add form for officer in detail report

--- src/components/detailReport.jsx
+++ src/components/detailReport.jsx
@@ -10,6 +10,7 @@ import {
   Popup,
   useMapEvent,
 } from 'react-leaflet';
+import { useDropzone } from 'react-dropzone';
 
 export default function DetailReport() {
   const auth = useSelector((state) => state.auth);
@@ -17,6 +18,14 @@ export default function DetailReport() {
   const [report, setReport] = useState([]);
   const [loading, setLoading] = useState(true);
   const [message, setMessage] = useState('');
+  const [reportOfficer, setReportOfficer] = useState({
+    message: '',
+    imageReport: [],
+  });
+  const [preview, setPreview] = useState([]);
+  let imageData = new FormData();
+
+  // console.log(report);
 
   const fetchData = async () => {
     try {
@@ -35,6 +44,7 @@ export default function DetailReport() {
   useEffect(() => {
     fetchData();
   }, [id, auth.token]);
+  console.log(report);
 
   function SetViewOnClick() {
     const map = useMapEvent('click', () => {
@@ -67,6 +77,99 @@ export default function DetailReport() {
     if (data.status === 'ok') {
       fetchData();
     }
+  };
+
+  const onDrop = async (acceptedFiles) => {
+    console.log(reportOfficer.imageReport.length);
+    if (reportOfficer.imageReport.length > 2) {
+      window.alert('maksimal bukti 3 foto');
+    } else {
+      acceptedFiles.forEach((file) => {
+        imageData.append('image', file);
+      });
+      const config = {
+        headers: {
+          Authorization: `Bearer ${auth.user ? auth.token : ''}`,
+        },
+      };
+      const uploadImage = await axios.post(
+        'http://localhost:5500/upload/image',
+        imageData,
+        config,
+      );
+      const getImage = uploadImage.data.image;
+      setReportOfficer({
+        ...reportOfficer,
+        imageReport: [...reportOfficer.imageReport, uploadImage.data.image],
+      });
+
+      const newUpload = {
+        image: [
+          getImage,
+          ...acceptedFiles.map((file) => URL.createObjectURL(file)),
+        ],
+      };
+      setPreview((prevUpload) => [...prevUpload, newUpload]);
+
+      console.log(reportOfficer);
+    }
+  };
+
+  const { getRootProps, getInputProps } = useDropzone({
+    onDrop,
+    accept: {
+      'image/png': ['.png'],
+      'image/jpg': ['.jpg'],
+      'image/jpeg': ['.jpeg'],
+    },
+    multiple: true,
+    maxFiles: 3,
+    maxSize: 2097152,
+  });
+
+  // Upload
+  const cancelUploadImage = async (itemIndex) => {
+    const imageName = preview[itemIndex].image[0];
+    console.log(reportOfficer);
+    const { data } = await axios.delete(
+      `http://localhost:5500/delete/image/${imageName}`,
+      {
+        headers: {
+          Authorization: `Bearer ${auth.user ? auth.token : ''}`,
+        },
+      },
+    );
+    console.log(data);
+    if (data.status === 'ok') {
+      const updatedUpload = preview.filter(
+        (item, index) => index !== itemIndex,
+      );
+      setPreview(updatedUpload);
+    } else {
+      window.alert('cancel upload image failed');
+    }
+
+    const updatedImageReport = [...report.imageReport];
+    updatedImageReport.splice(itemIndex, 1);
+
+    setReportOfficer({
+      ...reportOfficer,
+      imageReport: updatedImageReport,
+    });
+  };
+
+  const handleSubmit = async (e) => {
+    console.log(auth.token);
+    e.preventDefault();
+    const { data } = await axios.post(
+      `http://localhost:5500/officer/report/${report._id}`,
+      {
+        headers: {
+          Authorization: `Bearer ${auth.token}`,
+        },
+      },
+    );
+    console.log(data);
   };
 
   return (
@@ -156,6 +259,140 @@ export default function DetailReport() {
               </div>
             </div>
 
+            {report.unitWorks ? (
+              <div className="flex flex-col">
+                <label className="font-semibold text-gray-900">
+                  Unit Kerja:
+                </label>
+                <div>
+                  <img
+                    className="w-8 h-8 sm:w-10 sm:h-10 md:w-12 md:h-12 rounded-full group-hover:outline "
+                    src={`http://localhost:5500/public/image/${report.unitWorks.image}`}
+                    alt={report.unitWorks.name}
+                  />
+                  <p className="mb-4">{report.unitWorks.name}</p>
+                </div>
+              </div>
+            ) : null}
+
+            {report.officerReport ? (
+              <div>
+                <label className="font-semibold text-gray-900">
+                  Laporan Petugas:
+                </label>
+                <div>{report.officerReport.message}</div>
+                {report.officerReport.imageReport &&
+                  report.officerReport.imageReport.map((image, index) => (
+                    <img
+                      key={index}
+                      src={`http://localhost:5500/public/image/${image}`}
+                      alt="laporan petugas"
+                      className="h-36 w-fit mr-4 my-4"
+                      onError={(e) => {
+                        e.target.onerror = null;
+                        e.target.src = 'https://via.placeholder.com/150';
+                      }}
+                    />
+                  ))}
+              </div>
+            ) : null}
+            {/* Form Officer Report */}
+            {report.officerReport ? null : (
+              <div className="">
+                Buat Laporan
+                <div className="flex flex-col">
+                  <label className="block mb-2 text-xs md:text-base text-gray-900 font-semibold">
+                    Pesan:
+                  </label>
+                  <input
+                    type="text"
+                    name="title"
+                    value={reportOfficer.message}
+                    onChange={(e) =>
+                      setReportOfficer({
+                        ...reportOfficer,
+                        message: e.target.value,
+                      })
+                    }
+                    minLength="5"
+                    maxLength="50"
+                    required
+                    className="bg-gray-200 border border-gray-300 text-gray-900 text-sm rounded-lg focus:outline-none focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
+                  />
+                </div>
+                <div className="col-span-1">
+                  <div className="">
+                    <label className="block mb-2.5 text-xs md:text-base text-gray-900 font-semibold">
+                      Bukti Laporan:
+                    </label>
+                    <div
+                      {...getRootProps()}
+                      id="image-dropzone"
+                      className="border-2 border-dashed bg-gray-200 border-black p-5 mt-2"
+                    >
+                      <input {...getInputProps()} />
+                      <p className="text-center">
+                        Drag n drop some files here, or click to select files
+                      </p>
+                      <p className="text-center">
+                        Ukuran maksimal per file: 2MB
+                      </p>
+                      <p className="text-center">Bukti maskimal 3 foto</p>
+                    </div>
+                  </div>
+
+                  <div className="mt-4  mb-3 min-h-[11rem] h-fit">
+                    <label className="block mb-2 text-xs md:text-base text-gray-900 font-semibold ">
+                      Preview:
+                    </label>
+                    <div className="flex flex-col flex-wrap sm:flex-row">
+                      {preview &&
+                        preview.map((item, index) => (
+                          <div
+                            key={index}
+                            className="relative flex flex-col items-center space-x-2"
+                          >
+                            <img
+                              src={item.image[1]}
+                              alt={`Image ${index + 1}`}
+                              className="max-w-24 h-32 mx-4 my-2"
+                            />
+                            <button
+                              onClick={() => cancelUploadImage(index)}
+                              className="absolute -top-4 -right-4 bg-transparent rounded-md p-2 inline-flex items-center justify-center text-gray-500 hover: hover:text-red-700 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500"
+                            >
+                              <span className="sr-only">Hapus</span>
+                              <svg
+                                className="h-6 w-6"
+                                xmlns="http://www.w3.org/2000/svg"
+                                fill="none"
+                                viewBox="0 0 24 24"
+                                stroke="currentColor"
+                                aria-hidden="true"
+                              >
+                                <path
+                                  strokeLinecap="round"
+                                  strokeLinejoin="round"
+                                  strokeWidth="2"
+                                  d="M6 18L18 6M6 6l12 12"
+                                />
+                              </svg>
+                            </button>
+                          </div>
+                        ))}
+                    </div>
+                  </div>
+                  <div className="text-center mt:4 md:mt-8 w-full">
+                    <button
+                      className="bg-blue-600 text-white w-full p-2 rounded"
+                      onClick={(e) => handleSubmit(e)}
+                    >
+                      Kirim
+                    </button>
+                  </div>
+                </div>
+              </div>
+            )}
             <div className="w-full">
               <h3 className="font-bold text-primary-600 mb-4">
                 Komentar ({report.comment.length})
