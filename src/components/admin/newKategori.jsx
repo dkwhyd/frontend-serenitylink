d81873386b4c0823a839e5e4d490c6e362c0bd4a
dkwhyd <dikiwhyd99@gmail.com>
2023-12-05 17:03:05 +0700

    layout & disable log

--- src/components/admin/newKategori.jsx
+++ src/components/admin/newKategori.jsx
@@ -16,39 +16,36 @@ const NewKategori = () => {
   let imageData = new FormData();
 
   const onDrop = async (acceptedFiles) => {
-    // if (reportOfficer.imageReport.length > 1) {
-    //   window.alert('maksimal 1 foto');
-    // } else {
-    acceptedFiles.forEach((file) => {
-      imageData.append('image', file);
-    });
-    const config = {
-      headers: {
-        Authorization: `Bearer ${auth.user ? auth.token : ''}`,
-      },
-    };
-    const uploadImage = await axios.post(
-      `${import.meta.env.VITE_HOST_SERENITY}/upload/image`,
-      imageData,
-      config,
-    );
-    const getImage = uploadImage.data.image;
-    console.log(getImage);
-    setNewCategory({
-      ...newCategory,
-      image: getImage,
-    });
+    if (newCategory !== '') {
+      window.alert('maksimal 1 foto');
+    } else {
+      acceptedFiles.forEach((file) => {
+        imageData.append('image', file);
+      });
+      const config = {
+        headers: {
+          Authorization: `Bearer ${auth.user ? auth.token : ''}`,
+        },
+      };
+      const uploadImage = await axios.post(
+        `${import.meta.env.VITE_HOST_SERENITY}/upload/image`,
+        imageData,
+        config,
+      );
+      const getImage = uploadImage.data.image;
+      setNewCategory({
+        ...newCategory,
+        image: getImage,
+      });
 
-    const newUpload = {
-      image: [
-        getImage,
-        ...acceptedFiles.map((file) => URL.createObjectURL(file)),
-      ],
-    };
-    setPreview((prevUpload) => [...prevUpload, newUpload]);
-
-    console.log(newCategory);
-    // }
+      const newUpload = {
+        image: [
+          getImage,
+          ...acceptedFiles.map((file) => URL.createObjectURL(file)),
+        ],
+      };
+      setPreview((prevUpload) => [...prevUpload, newUpload]);
+    }
   };
 
   const { getRootProps, getInputProps } = useDropzone({
@@ -74,7 +71,7 @@ const NewKategori = () => {
         },
       },
     );
-    console.log(data);
+    // console.log(data);
     if (data.status === 'ok') {
       const updatedUpload = preview.filter(
         (item, index) => index !== itemIndex,
@@ -95,7 +92,7 @@ const NewKategori = () => {
 
   const handleSubmit = async (event) => {
     event.preventDefault();
-    console.log(newCategory);
+    // console.log(newCategory);
 
     try {
       const response = await axios.post(
@@ -109,7 +106,7 @@ const NewKategori = () => {
           },
         },
       );
-        console.log(response)
+      // console.log(response);
       if (response.data.status === 'ok') {
         alert('Kategori berhasil ditambahkan!');
         navigate('/dashboard/category');
@@ -122,100 +119,108 @@ const NewKategori = () => {
   };
 
   return (
-    <div>
-      <div className="w-5/6">
+    <div className="mx-auto">
+      <div className="animate__fadeIn animate__animated animate__delay-0.5s box-border rounded-3xl bg-white px-4 py-8 drop-shadow md:p-12 capitalize h-auto ">
         <div className="flex items-center mb-2 md:mb-4 mx-auto">
           <h2 className="md:text-4xl ml-4 text-lg font-semibold text-primary-600">
             Tambah Kategori
           </h2>
         </div>
-        <div className="flex flex-col">
-          <label className="block mb-2 text-xs md:text-base text-gray-900 font-semibold">
-            Nama Kategori
-          </label>
-          <input
-            type="text"
-            name="title"
-            value={newCategory.name}
-            onChange={(e) =>
-              setNewCategory({
-                ...newCategory,
-                name: e.target.value,
-              })
-            }
-            minLength="5"
-            maxLength="50"
-            required
-            className="bg-gray-200 border border-gray-300 text-gray-900 text-sm rounded-lg focus:outline-none focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
-          />
-        </div>
-        <div className="col-span-1">
-          <div className="">
-            <label className="block mb-2.5 text-xs md:text-base text-gray-900 font-semibold">
-              Bukti Laporan:
+        <div
+          data-aos="fade-zoom-in"
+          data-aos-easing="ease-in-back"
+          data-aos-duration="1000"
+          data-aos-delay="200"
+          data-aos-offset="0"
+        >
+          <div className="flex flex-col">
+            <label className="block mb-2 text-xs md:text-base text-gray-900 font-semibold">
+              Nama Kategori
             </label>
-            <div
-              {...getRootProps()}
-              id="image-dropzone"
-              className="border-2 border-dashed bg-gray-200 border-black p-5 mt-2"
-            >
-              <input {...getInputProps()} />
-              <p className="text-center">
-                Drag n drop some files here, or click to select files
-              </p>
-              <p className="text-center">Ukuran maksimal file: 2MB</p>
-              <p className="text-center"> Maskimal 1 foto</p>
-            </div>
+            <input
+              type="text"
+              name="title"
+              value={newCategory.name}
+              onChange={(e) =>
+                setNewCategory({
+                  ...newCategory,
+                  name: e.target.value,
+                })
+              }
+              minLength="5"
+              maxLength="50"
+              required
+              className="bg-gray-200 border border-gray-300 text-gray-900 text-sm rounded-lg focus:outline-none focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
+            />
           </div>
+          <div className="col-span-1">
+            <div className="">
+              <label className="block mb-2.5 text-xs md:text-base text-gray-900 font-semibold">
+                Bukti Laporan:
+              </label>
+              <div
+                {...getRootProps()}
+                id="image-dropzone"
+                className="border-2 border-dashed bg-gray-200 border-black p-5 mt-2"
+              >
+                <input {...getInputProps()} />
+                <p className="text-center">
+                  Drag n drop some files here, or click to select files
+                </p>
+                <p className="text-center">Ukuran maksimal file: 2MB</p>
+                <p className="text-center"> Maskimal 1 foto</p>
+              </div>
+            </div>
 
-          <div className="mt-4  mb-3 min-h-[11rem] h-fit">
-            <label className="block mb-2 text-xs md:text-base text-gray-900 font-semibold ">
-              Preview:
-            </label>
-            <div className="flex flex-col flex-wrap sm:flex-row">
-              {preview &&
-                preview.map((item, index) => (
-                  <div
-                    key={index}
-                    className="relative flex flex-col items-center space-x-2"
-                  >
-                    <img
-                      src={item.image[1]}
-                      alt={`Image ${index + 1}`}
-                      className="max-w-24 h-32 mx-4 my-2"
-                    />
-                    <button
-                      onClick={() => cancelUploadImage(index)}
-                      className="absolute -top-4 -right-4 bg-transparent rounded-md p-2 inline-flex items-center justify-center text-gray-500 hover: hover:text-red-700 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500"
+            <div className="mt-4 min-h-[11rem]">
+              <label className="block mb-2 text-xs md:text-base text-gray-900 font-semibold ">
+                Preview:
+              </label>
+              <div className="flex flex-col flex-wrap sm:flex-row">
+                {preview &&
+                  preview.map((item, index) => (
+                    <div
+                      key={index}
+                      className="relative flex flex-col items-center space-x-2"
                     >
-                      <span className="sr-only">Hapus</span>
-                      <svg
-                        className="h-6 w-6"
-                        xmlns="http://www.w3.org/2000/svg"
-                        fill="none"
-                        viewBox="0 0 24 24"
-                        stroke="currentColor"
-                        aria-hidden="true"
+                      <img
+                        src={item.image[1]}
+                        alt={`Image ${index + 1}`}
+                        className="max-w-24 h-32 mx-4 my-2"
+                      />
+                      <button
+                        onClick={() => cancelUploadImage(index)}
+                        className="absolute -top-4 -right-4 bg-transparent rounded-md p-2 inline-flex items-center justify-center text-gray-500 hover: hover:text-red-700 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500"
                       >
-                        <path
-                          strokeLinecap="round"
-                          strokeLinejoin="round"
-                          strokeWidth="2"
-                          d="M6 18L18 6M6 6l12 12"
-                        />
-                      </svg>
-                    </button>
-                  </div>
-                ))}
+                        <span className="sr-only">Hapus</span>
+                        <svg
+                          className="h-6 w-6"
+                          xmlns="http://www.w3.org/2000/svg"
+                          fill="none"
+                          viewBox="0 0 24 24"
+                          stroke="currentColor"
+                          aria-hidden="true"
+                        >
+                          <path
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            strokeWidth="2"
+                            d="M6 18L18 6M6 6l12 12"
+                          />
+                        </svg>
+                      </button>
+                    </div>
+                  ))}
+              </div>
             </div>
-          </div>
-          <div className="text-center mt:4 md:mt-8 w-full">
-            <button
-              className="bg-blue-600 mt-4 float-right py-2 px-6 rounded-lg focus:outline-none transition-all ease-out text-white hover:bg-blue-700 focus:bg-blue-900"
-              onClick={(e) => handleSubmit(e)}
-            >
-              Kirim
-            </button>
+            <div className="text-center md:mt-4 w-full">
+              <button
+                className="bg-blue-600 mt-4 float-right py-2 px-6 rounded-lg focus:outline-none transition-all ease-out text-white hover:bg-blue-700 focus:bg-blue-900"
+                onClick={(e) => handleSubmit(e)}
+              >
+                Kirim
+              </button>
+            </div>
           </div>
         </div>
       </div>
--- src/components/admin/newUnitWorks.jsx
+++ src/components/admin/newUnitWorks.jsx
@@ -116,100 +116,108 @@ const NewUnitWorks = () => {
   };
 
   return (
-    <div>
-      <div className="w-5/6">
+    <div className="mx-auto">
+      <div className="animate__fadeIn animate__animated animate__delay-0.5s box-border rounded-3xl bg-white px-4 py-8 drop-shadow md:p-12 capitalize h-auto ">
         <div className="flex items-center mb-2 md:mb-4 mx-auto">
           <h2 className="md:text-4xl ml-4 text-lg font-semibold text-primary-600">
-            Tambah Unit Kerja
+            Tambah Kategori
           </h2>
         </div>
-        <div className="flex flex-col">
-          <label className="block mb-2 text-xs md:text-base text-gray-900 font-semibold">
-            Nama Unit Kerja
-          </label>
-          <input
-            type="text"
-            name="title"
-            value={newUnitWorks.name}
-            onChange={(e) =>
-              setNewUnitWorks({
-                ...newUnitWorks,
-                name: e.target.value,
-              })
-            }
-            minLength="5"
-            maxLength="50"
-            required
-            className="bg-gray-200 border border-gray-300 text-gray-900 text-sm rounded-lg focus:outline-none focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
-          />
-        </div>
-        <div className="col-span-1">
-          <div className="">
-            <label className="block mb-2.5 text-xs md:text-base text-gray-900 font-semibold">
-              Bukti Laporan:
+        <div
+          data-aos="fade-zoom-in"
+          data-aos-easing="ease-in-back"
+          data-aos-duration="1000"
+          data-aos-delay="200"
+          data-aos-offset="0"
+        >
+          <div className="flex flex-col">
+            <label className="block mb-2 text-xs md:text-base text-gray-900 font-semibold">
+              Nama Unit Kerja
             </label>
-            <div
-              {...getRootProps()}
-              id="image-dropzone"
-              className="border-2 border-dashed bg-gray-200 border-black p-5 mt-2"
-            >
-              <input {...getInputProps()} />
-              <p className="text-center">
-                Drag n drop some files here, or click to select files
-              </p>
-              <p className="text-center">Ukuran maksimal file: 2MB</p>
-              <p className="text-center"> Maskimal 1 foto</p>
-            </div>
+            <input
+              type="text"
+              name="title"
+              value={newUnitWorks.name}
+              onChange={(e) =>
+                setNewUnitWorks({
+                  ...newUnitWorks,
+                  name: e.target.value,
+                })
+              }
+              minLength="5"
+              maxLength="50"
+              required
+              className="bg-gray-200 border border-gray-300 text-gray-900 text-sm rounded-lg focus:outline-none focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
+            />
           </div>
+          <div className="col-span-1">
+            <div className="">
+              <label className="block mb-2.5 text-xs md:text-base text-gray-900 font-semibold">
+                Bukti Laporan:
+              </label>
+              <div
+                {...getRootProps()}
+                id="image-dropzone"
+                className="border-2 border-dashed bg-gray-200 border-black p-1 mt-2"
+              >
+                <input {...getInputProps()} />
+                <p className="text-center">
+                  Drag n drop some files here, or click to select files
+                </p>
+                <p className="text-center">Ukuran maksimal file: 2MB</p>
+                <p className="text-center"> Maskimal 1 foto</p>
+              </div>
+            </div>
 
-          <div className="mt-4  mb-3 min-h-[11rem] h-fit">
-            <label className="block mb-2 text-xs md:text-base text-gray-900 font-semibold ">
-              Preview:
-            </label>
-            <div className="flex flex-col flex-wrap sm:flex-row">
-              {preview &&
-                preview.map((item, index) => (
-                  <div
-                    key={index}
-                    className="relative flex flex-col items-center space-x-2"
-                  >
-                    <img
-                      src={item.image[1]}
-                      alt={`Image ${index + 1}`}
-                      className="max-w-24 h-32 mx-4 my-2"
-                    />
-                    <button
-                      onClick={() => cancelUploadImage(index)}
-                      className="absolute -top-4 -right-4 bg-transparent rounded-md p-2 inline-flex items-center justify-center text-gray-500 hover: hover:text-red-700 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500"
+            <div className="mt-4  mb-3 min-h-[11rem] h-fit">
+              <label className="block mb-2 text-xs md:text-base text-gray-900 font-semibold ">
+                Preview:
+              </label>
+              <div className="flex flex-col flex-wrap sm:flex-row">
+                {preview &&
+                  preview.map((item, index) => (
+                    <div
+                      key={index}
+                      className="relative flex flex-col items-center space-x-2"
                     >
-                      <span className="sr-only">Hapus</span>
-                      <svg
-                        className="h-6 w-6"
-                        xmlns="http://www.w3.org/2000/svg"
-                        fill="none"
-                        viewBox="0 0 24 24"
-                        stroke="currentColor"
-                        aria-hidden="true"
+                      <img
+                        src={item.image[1]}
+                        alt={`Image ${index + 1}`}
+                        className="max-w-24 h-32 mx-4 my-2"
+                      />
+                      <button
+                        onClick={() => cancelUploadImage(index)}
+                        className="absolute -top-4 -right-4 bg-transparent rounded-md p-2 inline-flex items-center justify-center text-gray-500 hover: hover:text-red-700 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500"
                       >
-                        <path
-                          strokeLinecap="round"
-                          strokeLinejoin="round"
-                          strokeWidth="2"
-                          d="M6 18L18 6M6 6l12 12"
-                        />
-                      </svg>
-                    </button>
-                  </div>
-                ))}
+                        <span className="sr-only">Hapus</span>
+                        <svg
+                          className="h-6 w-6"
+                          xmlns="http://www.w3.org/2000/svg"
+                          fill="none"
+                          viewBox="0 0 24 24"
+                          stroke="currentColor"
+                          aria-hidden="true"
+                        >
+                          <path
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            strokeWidth="2"
+                            d="M6 18L18 6M6 6l12 12"
+                          />
+                        </svg>
+                      </button>
+                    </div>
+                  ))}
+              </div>
             </div>
-          </div>
-          <div className="text-center mt:4 md:mt-8 w-full">
-            <button
-              className="bg-blue-600 mt-4 float-right py-2 px-6 rounded-lg focus:outline-none transition-all ease-out text-white hover:bg-blue-700 focus:bg-blue-900"
-              onClick={(e) => handleSubmit(e)}
-            >
-              Kirim
-            </button>
+            <div className="text-center mt:4 md:mt-8 w-full">
+              <button
+                className="bg-blue-600 mt-4 float-right py-2 px-6 rounded-lg focus:outline-none transition-all ease-out text-white hover:bg-blue-700 focus:bg-blue-900"
+                onClick={(e) => handleSubmit(e)}
+              >
+                Kirim
+              </button>
+            </div>
           </div>
         </div>
       </div>
