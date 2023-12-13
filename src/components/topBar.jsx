a3fe463e90aa07ac3a602003521e6933f0ff3847
dkwhyd <dikiwhyd99@gmail.com>
2023-12-13 21:36:56 +0700

    made menu sticky on top

--- src/components/topBar.jsx
+++ src/components/topBar.jsx
@@ -13,38 +13,36 @@ export default function TopBar() {
     setDropdownOpen(!isDropdownOpen);
   };
   return (
-    <>
-      <div className="relative">
-        <div
-          className="flex flex-row text-center items-center text-white cursor-pointer"
-          onClick={handleToggleDropdown}
-        >
-          {auth.user?.name}
-          <button className="p-2 ">
-            <FaUser />
-          </button>
-        </div>
-        {isDropdownOpen && (
-          <div className="absolute -left-20 bg-white p-2 rounded shadow mt-2 z-40">
-            <div className="flex flex-col w-32">
-              <Link
-                to={'/password'}
-                className=" flex items-center block w-full text-left  font-bold mb-2 hover:bg-blue-500"
-              >
-                <RiLockPasswordFill />
-                Password
-              </Link>
-              <Link
-                to={'/logout'}
-                className=" flex items-center block w-full text-left text-red-500 font-bold"
-              >
-                <FiLogOut />
-                Logout
-              </Link>
-            </div>
-          </div>
-        )}
+    <div className="relative top-0 sticky">
+      <div
+        className="flex flex-row text-center items-center text-white cursor-pointer"
+        onClick={handleToggleDropdown}
+      >
+        {auth.user?.name}
+        <button className="p-2 ">
+          <FaUser />
+        </button>
       </div>
-    </>
+      {isDropdownOpen && (
+        <div className="absolute -left-20 bg-white p-2 rounded shadow mt-2 z-40">
+          <div className="flex flex-col w-32">
+            <Link
+              to={'/password'}
+              className=" flex items-center block w-full text-left  font-bold mb-2 hover:bg-blue-500"
+            >
+              <RiLockPasswordFill />
+              Password
+            </Link>
+            <Link
+              to={'/logout'}
+              className=" flex items-center block w-full text-left text-red-500 font-bold"
+            >
+              <FiLogOut />
+              Logout
+            </Link>
+          </div>
+        </div>
+      )}
+    </div>
   );
 }
